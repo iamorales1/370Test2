fd86ceaa4d66104dd9ec2dd7efae2e8d593caf9a
eichavez <eichavez@mail.csuchico.edu>
2022-04-28 11:34:37 -0700

    Stuff
    I made Some stuff
                            EC

---
+++ 370Test2/Assets/Scripts/TestScriptE.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //I added This too
+            //Ec
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
